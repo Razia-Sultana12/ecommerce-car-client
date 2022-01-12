5bf4150f306513441b6b07fe84d46fa0cbda9427
Razia Sultana Mun <nisridhika@gmail.com>
2022-01-12 15:28:04 +0600

    Update useFirebase.js

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -72,7 +72,7 @@ const useFirebase = () => {
     }, [])
 
     useEffect(() => {
-        fetch(`http://localhost:5000/users/${user.email}`)
+        fetch(`https://fierce-peak-59128.herokuapp.com/users/${user.email}`)
             .then(res => res.json())
             .then(data => setAdmin(data.admin))
     }, [user.email])
@@ -90,7 +90,7 @@ const useFirebase = () => {
     }
     const saveUser = (email, displayName) => {
         const user = { email, displayName };
-        fetch('http://localhost:5000/users', {
+        fetch('https://fierce-peak-59128.herokuapp.com/users', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
